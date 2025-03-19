cee0fbc5b5987b8fece33c55878af2471f5c3fe9
arifulislamk <ariful18374@gmail.com>
2025-03-19 21:23:03 +0600

    project start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className=' text-4xl text-red-800 font-extrabold'>Hello , Ariful islam Khan. are you alive???</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/layout/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h1>Welcome to The Smart Food Service System</h1>
+            <p>This is a website for managing and delivering food in a smart and efficient manner.</p>
+            <p>Please feel free to explore the website and let us know what you think.</p>
+        </div>
+    );
+};
+
+export default Main;
